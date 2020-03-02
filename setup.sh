b957f9ca2127e812ee7e995f1c011796cc974969
jeg <jugheadeatsalot@gmail.com>
2020-03-02 02:11:41 -0800

    ...

--- setup.sh
+++ setup.sh
@@ -2,7 +2,6 @@
 
 . ./.env
 
-devport=''
 network=''
 
 if [ ! -z ${NETWORKS_DEFAULT_EXTERNAL_NAME} ]
@@ -17,44 +16,38 @@ networks:
       name: ${NETWORKS_DEFAULT_EXTERNAL_NAME}
 EOF
 )"
-else
-devport="$(
-cat <<EOF
-
-    ports:
-      '${DEV_PORT-8282}:80'
-EOF
-)"
 fi
 
 cat > docker-compose.yml <<EOF
 version: '3.3'
 
 services:
-  ${COMPOSE_PROJECT_DOMAIN}:
+  ${PHP_DOMAIN}:
     image: nginx:alpine
     restart: unless-stopped
-    container_name: ${COMPOSE_PROJECT_NAME}_nginx
+    container_name: ${PHP_DOMAIN}
     depends_on:
-      - ${COMPOSE_PROJECT_NAME}_php
+      - ${PHP_ID}_php
     volumes:
+      - ./ncache:/ncache
       - ./www:/var/www/html
       - ./nginx/logs:/var/log/nginx
       - ./nginx/default.conf:/etc/nginx/default.template
       - ./nginx/extras:/etc/nginx/extras
     environment:
-      PHP_HOST: ${COMPOSE_PROJECT_NAME}_php:9000
-      VIRTUAL_HOST: ${COMPOSE_PROJECT_DOMAIN}
-      LETSENCRYPT_HOST: ${COMPOSE_PROJECT_DOMAIN}
+      PHP_HOST: ${PHP_ID}_php:9000
+      VIRTUAL_HOST: ${PHP_DOMAIN}
+      LETSENCRYPT_HOST: ${PHP_DOMAIN}
     command: >
       /bin/sh -c "envsubst '\$\$PHP_HOST'
       < /etc/nginx/default.template >
       /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"
-  ${COMPOSE_PROJECT_NAME}_php:
+  ${PHP_ID}_php:
     build: php
     restart: unless-stopped
-    container_name: ${COMPOSE_PROJECT_NAME}_php${devport}
+    container_name: ${PHP_ID}_php
     volumes:
+      - ./ncache:/ncache
       - ./www:/var/www/html
       - ./default.conf:/etc/nginx/conf.d/${network}
 EOF
